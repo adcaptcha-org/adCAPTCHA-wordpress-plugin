ec108895c8782a45626f86287e0a00b3b1095435
Istvan Vas <swannxxx@yahoo.co.uk>
2024-12-04 12:06:32 +0000

    Update phpunit.xml

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,19 +1,10 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Suppress deprecations (temporary measure)
-error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
-
-// Set up Brain Monkey
 if (class_exists('\Brain\Monkey')) {
     \Brain\Monkey\setUp();
 
-    // Ensure proper teardown on shutdown
     register_shutdown_function(function () {
         \Brain\Monkey\tearDown();
     });
 }
-
-// Debugging (remove in production/testing CI once verified)
-// var_dump(getcwd());
-// var_dump(realpath(__DIR__ . '/../vendor/autoload.php'));
