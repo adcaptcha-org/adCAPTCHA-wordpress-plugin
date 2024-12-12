539328a1bbd04f8eabf50a96d356267beff0cd99
Istvan Vas <swannxxx@yahoo.co.uk>
2024-12-12 15:49:20 +0000

    Forms.php updated require_once removed as we using autoloder. Update the dependency injection for Verify. Create new helper function in the test_helpers.php . WPFormsTest.php test is done using different mock technics.

--- tests/test_helpers.php
+++ tests/test_helpers.php
@@ -19,3 +19,33 @@ if(!function_exists('remove_action')) {
         $mocked_remove_actions[] = compact('hook', 'callback', 'priority', 'accepted_args');
     }
 }
+
+function execute_mocked_hook($hook_name) {
+    global $mocked_actions;
+
+    foreach ($mocked_actions as $action) {
+        if ($action['hook'] === $hook_name) {
+            if (is_callable($action['callback'])) {
+                call_user_func($action['callback']);
+            } else {
+                error_log("Callback for hook {$hook_name} is not callable.");
+            }
+        }
+    }
+}
+
+// Helper function to check hooks in mocked actions or filters
+function check_hook_registration($mocked_hooks, $hook_name, $priority = 10, $accepted_args = 1) {
+    foreach ($mocked_hooks as $hook) {
+        if (
+            isset($hook['hook'], $hook['callback'], $hook['priority'], $hook['accepted_args']) &&
+            $hook['hook'] === $hook_name &&
+            $hook['priority'] === $priority &&
+            $hook['accepted_args'] === $accepted_args &&
+            is_callable($hook['callback'])
+        ) {
+            return true;
+        }
+    }
+    return false;
+}
