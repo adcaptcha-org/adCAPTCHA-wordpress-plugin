144140d953da8aef6fa4c5b23972f04b48223977
Istvan Vas <swannxxx@yahoo.co.uk>
2025-03-03 14:44:26 +0000

    Forminator automatic integration is done

--- src/Plugin/Forminator/Forms.php
+++ src/Plugin/Forminator/Forms.php
@@ -18,28 +18,52 @@ class Forms extends AdCaptchaPlugin {
 
     public function setup() {
         add_action('forminator_before_form_render', [$this, 'before_form_render'], 10, 5);
-        add_action('forminator_before_form_render', [AdCaptcha::class, 'enqueue_scripts']);
-        add_action('forminator_before_form_render', [Verify::class, 'get_success_token']);
-
+        add_action('forminator_before_form_render', [AdCaptcha::class, 'enqueue_scripts'], 9);
+        add_action('forminator_before_form_render', [Verify::class, 'get_success_token'], 9);
         add_filter('forminator_render_button_markup', [$this, 'captcha_trigger_filter'], 10, 2);
+        add_action( 'wp_enqueue_scripts', [ $this, 'reset_captcha_script' ], 9 );
         add_filter('forminator_cform_form_is_submittable', [$this, 'verify'], 10, 3);
-        add_filter('forminator_custom_form_fields', [$this, 'register_adcaptcha_field']);
     }
 
     public function before_form_render($id, $form_type, $post_id, $form_fields, $form_settings) {
         $this->has_captcha = $this->has_adcaptcha_field($form_fields);
     }
 
-    public function register_adcaptcha_field($fields) {
-        $fields['adcaptcha'] = [
-            'field'      => 'AdCaptcha',
-            'icon'       => 'dashicons-shield', 
-            'position'   => 'basic',
-            'class'      => 'forminator-custom-adcaptcha',
-            'template'   => 'adcaptcha',
-            'settings'   => [$this, 'adcaptcha_settings_template'],
-        ];
-        return $fields;
+    private function has_adcaptcha_field($form_fields) {
+        foreach ($form_fields as $field) {
+            if (!empty($field['type']) && $field['type'] === 'captcha' && !empty($field['captcha_provider']) && $field['captcha_provider'] === 'adcaptcha') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function captcha_trigger_filter($html, string $button) {
+        if ($this->has_captcha) {
+            return $html;
+        }
+        return AdCaptcha::ob_captcha_trigger() . $html;
+    }
+
+
+    public function reset_captcha_script() {
+        add_action('wp_footer', function() {
+            echo "<script>
+                (function checkJQuery() {
+                    if (typeof jQuery === 'undefined') {
+                        setTimeout(checkJQuery, 100);
+                        return;
+                    }
+                    jQuery(document).on('after:forminator:form:submit', function() {
+                        if (window.adcap && typeof window.adcap.reset === 'function') {
+                            window.adcap.reset();
+                            window.adcap.successToken = '';
+                            jQuery('[name=\"adcaptcha_successToken\"]').val('');
+                        }   
+                    });
+                })();
+            </script>";
+        });
     }
 
     public function verify($can_show, $form_id, $field_data_array) {
@@ -67,22 +91,5 @@ class Forms extends AdCaptchaPlugin {
 
         $this->verified = true;
         return $can_show;
-    }
-
-    public function captcha_trigger_filter($html, string $button) {
-        if ($this->has_captcha) {
-            return $html;
-        }
-
-        return AdCaptcha::ob_captcha_trigger() . $html;
-    }
-
-    private function has_adcaptcha_field($form_fields) {
-        foreach ($form_fields as $field) {
-            if (!empty($field['type']) && $field['type'] === 'captcha' && !empty($field['captcha_provider']) && $field['captcha_provider'] === 'adcaptcha') {
-                return true;
-            }
-        }
-        return false;
     }
 }
