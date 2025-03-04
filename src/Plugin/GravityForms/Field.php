76db999eef2f45afd2e34fbfb818a1549af01ef5
Istvan Vas <swannxxx@yahoo.co.uk>
2025-03-04 11:39:52 +0000

    Fixed comment from PR. For readability, place functions in order.

--- src/Plugin/GravityForms/Field.php
+++ src/Plugin/GravityForms/Field.php
@@ -36,11 +36,9 @@ class Field extends GF_Field {
         try {
             if (!GF_Fields::get('adcaptcha')) {
                 GF_Fields::register($this);
-            } else {
-                error_log('⚠️ AdCaptcha field already registered.');
-            }
+            } 
         } catch (Exception $e) {
-            error_log('❌ Error registering AdCaptcha field: ' . $e->getMessage());
+            throw $e;
         }
 
         $this->setup_hooks();
@@ -50,35 +48,140 @@ class Field extends GF_Field {
         add_action('wp_enqueue_scripts', [AdCaptcha::class, 'enqueue_scripts'], 9);
         add_action( 'wp_enqueue_scripts', [ Verify::class, 'get_success_token' ] );
         add_filter('gform_field_groups_form_editor', [$this, 'add_to_field_groups']);
-        add_filter('gform_field_content', function ($content, $field) {
-            if ($field->type === 'adcaptcha') {
-                return str_replace($field->get_field_label(false, ''), '', $content);
-            }
-            return $content;
-        }, 10, 2);
+        add_filter('gform_field_content', [$this, 'modify_gform_field_content'], 10, 2);
         add_filter('gform_validation', [$this, 'verify_captcha']);
         add_action('admin_head', [$this, 'custom_admin_field_icon_style']);
         add_action('admin_init', [$this, 'update_adcaptcha_label']);
         add_action('admin_footer', [$this, 'enqueue_admin_script']);
-        add_filter('gform_pre_render', function($form) {
-            if (!empty($_POST['adcaptcha_successToken'])) {
-                echo "<script>
-                    document.addEventListener('DOMContentLoaded', function() {
-                        let adCaptchaField = document.querySelector('.adcaptcha_successToken');
-                        if (adCaptchaField) {
-                            setTimeout(function() {
-                                if (window.adcap) {
-                                    window.adcap.setVerificationState('success');
-                                     adCaptchaField.value = '" . esc_js($_POST['adcaptcha_successToken']) . "';
-                                }
-                            }, 500);
-                        }
-                    });
-                </script>";
-            }
-            return $form;
-        });
+        add_filter('gform_pre_render', [$this, 'handle_adcaptcha_token']);
         add_action('gform_preview_body_open', [$this, 'enqueue_preview_scripts']);
+    }
+
+    public function add_to_field_groups($field_groups): array {
+        foreach ($field_groups['advanced_fields']['fields'] as $field) {
+            if ($field['data-type'] === 'adcaptcha') {
+                return $field_groups;
+            }
+        }
+        $field_groups['advanced_fields']['fields'][] = [
+            'data-type' => $this->type,
+            'value'     => $this->get_form_editor_field_title(),
+            'label'     => $this->get_form_editor_field_title(),
+        ];
+    
+        return $field_groups;
+    }
+
+    public function modify_gform_field_content($content, $field) {
+        if ($field->type === 'adcaptcha') {
+            return str_replace($field->get_field_label(false, ''), '', $content);
+        }
+        return $content;
+    }
+
+    public function verify_captcha($validation_result) {
+        $form = $validation_result['form'];
+        $is_valid = $validation_result['is_valid'];
+        foreach ($form['fields'] as &$field) {
+            if ($field->type === 'adcaptcha') {
+                $successToken = sanitize_text_field(wp_unslash($_POST['adcaptcha_successToken'] ?? ''));
+                if (empty($successToken) || trim($successToken) === '') {
+                    $field->failed_validation = true;
+                    $field->validation_message = __('Incomplete CAPTCHA, Please try again.', 'adcaptcha');
+                    $is_valid = false;
+                } 
+                if($is_valid) {
+                    $response = $this->verify->verify_token($successToken);
+                    if (!$response) {
+                        $field->failed_validation = true;
+                        $field->validation_message = __('Invalid token.', 'adcaptcha');
+                        $is_valid = false;
+                    }
+                } 
+            } 
+        }
+        $validation_result['is_valid'] = $is_valid; 
+        $validation_result['form'] = $form;
+        return $validation_result;
+    }
+
+    public function custom_admin_field_icon_style() {
+        echo '<style>
+            #sidebar_field_info #sidebar_field_icon img {
+                width: 16px !important; 
+            }
+        </style>';
+    }
+
+    public function update_adcaptcha_label() {
+        $forms = GFAPI::get_forms();
+        if (!$forms || !is_array($forms)) {
+            return;
+        }
+        $errors = [];
+        foreach ($forms as $form) {
+            $updated = false;
+            foreach ($form['fields'] as &$field) {
+                if ($field->type === 'adcaptcha' && $field->label !== __('adCAPTCHA', 'adcaptcha')) {
+                    $field->label = __('adCAPTCHA', 'adcaptcha');
+                    $field['label'] = __('adCAPTCHA', 'adcaptcha');
+                    $updated = true;
+                }
+            }
+            if ($updated) {
+                $result = GFAPI::update_form($form);
+    
+                if (is_wp_error($result)) {
+                    $errors[] = "Failed to update adCAPTCHA label in Form ID {$form['id']}: " . $result->get_error_message();
+                }
+            }
+        }
+        if (!empty($errors)) {
+            throw new Exception(implode("\n", $errors));
+        }
+    }
+
+    public function enqueue_admin_script() {
+        ?>
+        <script>
+            (function($){
+                if (typeof window.CanFieldBeAdded !== 'function') {
+                    return;
+                }
+                let originalFunction = window.CanFieldBeAdded;
+                window.CanFieldBeAdded = function(type) {
+                    if (type === "adcaptcha") {
+                        if (GetFieldsByType(["adcaptcha"]).length > 0) {
+                            gform.instances.dialogAlert("Field Error", "Only one adCAPTCHA field can be added.");
+                            return false;
+                        }
+                    }
+                    return originalFunction(type);
+                };
+            })(jQuery);
+        </script>
+        <?php
+    }
+
+    public function handle_adcaptcha_token($form) {
+        if (!empty($_POST['adcaptcha_successToken'])) {
+            $successToken = esc_js($_POST['adcaptcha_successToken']);
+    
+            echo "<script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    let adCaptchaField = document.querySelector('.adcaptcha_successToken');
+                    if (adCaptchaField) {
+                        setTimeout(function() {
+                            if (window.adcap) {
+                                window.adcap.setVerificationState('success');
+                                adCaptchaField.value = '{$successToken}';
+                            }
+                        }, 500);
+                    }
+                });
+            </script>";
+        }
+        return $form;
     }
 
     public function enqueue_preview_scripts($form_id) {
@@ -117,42 +220,12 @@ class Field extends GF_Field {
         return $input;
     }
 
-    public function update_adcaptcha_label() {
-        $forms = GFAPI::get_forms();
-        if (!$forms || !is_array($forms)) {
-            return;
-        }
-        foreach ($forms as $form) {
-            foreach ($form['fields'] as &$field) {
-                if ($field->type === 'adcaptcha') {
-                    $field->label = __('adCAPTCHA', 'adcaptcha');
-                    $field['label'] = __('adCAPTCHA', 'adcaptcha');
-                    $result = GFAPI::update_form($form);
-                    if (is_wp_error($result)) {
-                        error_log("❌ Failed to update adCAPTCHA label in Form ID {$form['id']}: " . $result->get_error_message());
-                    } else {
-                        error_log("✅ Successfully updated adCAPTCHA label in Form ID {$form['id']}.");
-                    }
-                    return;
-                }
-            }
-        }
-    }
-
     public function get_form_editor_field_title() {
         return esc_html__('adCAPTCHA', 'adcaptcha');
     }
 
     public function get_form_editor_field_settings() {
         return [ 'description_setting', 'error_message_setting', 'label_placement_setting', 'css_class_setting',];
-    }
-
-    function custom_admin_field_icon_style() {
-        echo '<style>
-            #sidebar_field_info #sidebar_field_icon img {
-                width: 16px !important; 
-            }
-        </style>';
     }
 
     public function get_form_editor_field_description() {
@@ -164,68 +237,5 @@ class Field extends GF_Field {
 
     public function get_form_editor_field_icon() {
         return plugin_dir_url( __FILE__ ) . '../../../assets/adcaptcha_icon.png'; 
-    }
-
-    public function add_to_field_groups($field_groups): array {
-        foreach ($field_groups['advanced_fields']['fields'] as $field) {
-            if ($field['data-type'] === 'adcaptcha') {
-                return $field_groups;
-            }
-        }
-        $field_groups['advanced_fields']['fields'][] = [
-            'data-type' => $this->type,
-            'value'     => $this->get_form_editor_field_title(),
-            'label'     => $this->get_form_editor_field_title(),
-        ];
-    
-        return $field_groups;
-    }
-
-    public function enqueue_admin_script() {
-        ?>
-        <script>
-            (function($){
-                if (typeof window.CanFieldBeAdded !== 'function') {
-                    return;
-                }
-                let originalFunction = window.CanFieldBeAdded;
-                window.CanFieldBeAdded = function(type) {
-                    if (type === "adcaptcha") {
-                        if (GetFieldsByType(["adcaptcha"]).length > 0) {
-                            gform.instances.dialogAlert("Field Error", "Only one adCAPTCHA field can be added.");
-                            return false;
-                        }
-                    }
-                    return originalFunction(type);
-                };
-            })(jQuery);
-        </script>
-        <?php
-    }
-
-    public function verify_captcha($validation_result) {
-        $form = $validation_result['form'];
-        $is_valid = $validation_result['is_valid'];
-        foreach ($form['fields'] as &$field) {
-            if ($field->type === 'adcaptcha') {
-                $successToken = sanitize_text_field(wp_unslash($_POST['adcaptcha_successToken'] ?? ''));
-                if (empty($successToken) || trim($successToken) === '') {
-                    $field->failed_validation = true;
-                    $field->validation_message = __('Incomplete CAPTCHA, Please try again.', 'adcaptcha');
-                    $is_valid = false;
-                } 
-                if($is_valid) {
-                    $response = $this->verify->verify_token($successToken);
-                    if (!$response) {
-                        $field->failed_validation = true;
-                        $field->validation_message = __('Invalid token.', 'adcaptcha');
-                        $is_valid = false;
-                    }
-                } 
-            } 
-        }
-        $validation_result['is_valid'] = $is_valid; 
-        $validation_result['form'] = $form;
-        return $validation_result;
     }
 }
